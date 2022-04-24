263ef75b06f323478714e90c6a1a691dcf4e462d
Владислав <rogatihvlad@gmail.com>
2022-04-24 18:06:32 +0300

    Доработка меню бургер, добавление компонентов формы, создание форм регистрации и авторизации, доработка аккордеона в терии, настройка маршрутов для регистрации

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        dd($request);
+    }
+
+    public function regis()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
